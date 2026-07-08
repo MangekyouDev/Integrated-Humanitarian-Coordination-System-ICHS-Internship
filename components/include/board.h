38f4f0afd2d2d322a18179f23d947c16f7b63179
BayHarborDeveloper <haythemsassi15@gmail.com>
2026-07-08 11:28:21 +0100

    Add board pin definitions header

---
+++ components/include/board.h
@@ -0,0 +1,14 @@
+#ifndef BOARD_H
+#define BOARD_H
+
+#include "driver/gpio.h"
+
+/* LCD I2C pins */
+#define LCD_SDA_PIN GPIO_NUM_21
+#define LCD_SCL_PIN GPIO_NUM_22
+
+/* Buttons */
+#define BUTTON_1_PIN GPIO_NUM_32
+#define BUTTON_2_PIN GPIO_NUM_33
+
+#endif
